57946c6c096cadb7e9c228825a7fc3a5e10142c5
Jacob Snarr <jacobsnarr@gmail.com>
2019-01-28 04:05:25 -0500

    Fix: Fixed Client & Server Code, Both Work

--- index.js
+++ index.js
@@ -6,26 +6,29 @@ let config = {};
 
 fs.readFile('config.json', 'utf8', (err, data) => {
 	config = JSON.parse(data);
+	connect();
 })
 
-let client = new net.Socket();
+function connect() {
+	let client = new net.Socket();
 
-client.connect(port, ip, function() {
-	console.log('Connected');
-});
-
-client.on('error', error => {
-	console.error;
-})
-
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
-process.stdin.on('keypress', (str, key) => {
-  if (key.ctrl && key.name === 'c') {
-	process.exit();
-  } else {
-	// console.log(key.name);
-	client.write(key.name);
-  }
-});
-console.log('Press any key...');
+	client.connect(config.port, config.ip, function() {
+		console.log('Connected');
+	});
+	
+	client.on('error', error => {
+		console.error;
+	})
+	
+	readline.emitKeypressEvents(process.stdin);
+	process.stdin.setRawMode(true);
+	process.stdin.on('keypress', (str, key) => {
+	  if (key.ctrl && key.name === 'c') {
+		process.exit();
+	  } else {
+		// console.log(key.name);
+		client.write(key.name);
+	  }
+	});
+	console.log('Press any key...');
+}
--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const net = require('net');
+const fs = require('fs');
 
 let config = {};
 
 fs.readFile('config.json', 'utf8', (err, data) => {
 	config = JSON.parse(data);
+	startServer();
 })
 
-net.createServer(function (socket) {
+function startServer() {
+	net.createServer(function (socket) {
 
-	// Identify this client
-	socket.name = socket.remoteAddress + ":" + socket.remotePort 
-  
-	// Handle incoming messages from clients.
-	socket.on('data', function (data) {
-		console.log(data.toString('utf8'));
-	});
-  
-	// Remove the client from the list when it leaves
-	socket.on('end', function () {
-	  clients.splice(clients.indexOf(socket), 1);
-	});
-  
-}).listen(config.port);
+		// Identify this client
+		socket.name = socket.remoteAddress + ":" + socket.remotePort 
+	  
+		// Handle incoming messages from clients.
+		socket.on('data', function (data) {
+			console.log(data.toString('utf8'));
+		});
+	  
+		// Remove the client from the list when it leaves
+		socket.on('end', function () {
+		  clients.splice(clients.indexOf(socket), 1);
+		});
+	
+	}).listen(config.port);
+}
